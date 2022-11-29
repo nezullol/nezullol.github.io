31059f5bd4602cfcfb8434bef46d8d89f86ebe3e
nezullol <mchmav@gmail.com>
2022-11-29 17:21:06 -0600

    got the html skeleton built

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,13 +6,19 @@ import '../App.css';
 function About() {
   return (
     <div className="About">
-      ABOUT
-      INFO ABOUT HOW THE OWNER
-      PORTFOLIO Link
-      BLOG
-      PERSONAL PROJECTS
-    <div> Information card about company </div>
-    <div> parallax background</div>
+      <ul> 
+
+<ul>ABOUT</ul> <br />
+<ul>PERSONAL PROJECTS</ul>
+<ul>INFO ABOUT HOW THE OWNER</ul>
+<ul>PORTFOLIO Link</ul>
+<ul>BLOG</ul>
+      </ul>
+<ul>
+
+    <ul> Information card about company </ul>
+    <ul> parallax background</ul>
+</ul>
 
     </div>
   )
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,10 +6,15 @@ import '../App.css';
 function Contact() {
   return (
     <div className="Contact">
-      CONTACT
-    <div> Information card about company </div>
-    <div> parallax background</div>
-
+      <ul>CONTACT</ul>
+      <ul>Address</ul>
+      <ul>Phone Number</ul>
+      <ul>Business Hours</ul>
+      <ul>Emails</ul>
+      <ul>    
+        <ul> Information card about company </ul>
+    <ul> Parallax background</ul>
+    </ul>
     </div>
   )
 }
